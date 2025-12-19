b1df52ef72f2d1a635f24ee5e400b2c6d37a320d
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2025-12-19 15:47:12 +0530

    chore(ui): update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import { Hanken_Grotesk as FontSans } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { user } from "@/data/user";
+import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   display: "swap",
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -20,12 +22,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className={fontSans.className} lang="en">
-      <body className="mx-auto flex w-full max-w-280 flex-col px-sm py-xl antialiased md:flex-row md:py-20">
+    <html className={cn(fontSans.variable, "antialiased")} lang="en">
+      <body
+        className={cn(
+          "bg-bg-1 text-[15px] text-fg-3 leading-[1.65] selection:bg-brand/10",
+          // Layout
+          "mx-auto flex w-full max-w-260 flex-col px-sm py-xl md:flex-row md:py-7xl"
+        )}
+      >
         <div className="flex flex-1 justify-start">
           <Navbar />
         </div>
-        <main className="mt-md flex flex-3 flex-col gap-xl md:mt-0">
+        <main className="mt-md flex flex-4 flex-col gap-xl md:mt-0">
           {children}
         </main>
         <div className="hidden flex-1 justify-end md:flex" />
