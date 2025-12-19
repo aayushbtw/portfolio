63ec91a4eabf175815117283ed3d7d69f74241df
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2025-12-19 15:46:33 +0530

    chore(ui): update component `card`

--- components/card.tsx
+++ components/card.tsx
@@ -1,25 +1,44 @@
 import { cn } from "@/lib/utils";
 
 function Card({ className, ...props }: React.ComponentProps<"div">) {
-  return <div className={cn("flex flex-col gap-y-sm", className)} {...props} />;
+  return (
+    <div
+      className={cn("flex flex-col gap-y-sm", className)}
+      data-slot="card"
+      {...props}
+    />
+  );
 }
 
 function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
-  return <div className={className} {...props} />;
+  return <div className={className} data-slot="card-header" {...props} />;
 }
 
 function CardTitle({ className, ...props }: React.ComponentProps<"h1">) {
-  return <h1 className={cn("heading", className)} {...props} />;
+  return (
+    <h1
+      className={cn("heading", className)}
+      data-slot="card-title"
+      {...props}
+    />
+  );
 }
 
 function CardDescription({ className, ...props }: React.ComponentProps<"h2">) {
-  return <h2 className={cn("subheading", className)} {...props} />;
+  return (
+    <h2
+      className={cn("subheading", className)}
+      data-slot="card-description"
+      {...props}
+    />
+  );
 }
 
 function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       className={cn("paragraph flex flex-col gap-y-xs", className)}
+      data-slot="card-content"
       {...props}
     />
   );
