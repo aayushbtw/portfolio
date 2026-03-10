03d12c13be918e3fbbe6da146f6d10b3e718447e
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2026-03-10 23:45:22 +0530

    feat(ux): add custom cursor for external links

--- components/list-projects.tsx
+++ components/list-projects.tsx
@@ -35,6 +35,8 @@ function ListProjects() {
           <Link
             className="flex items-center gap-4 py-2.5 text-sm transition-opacity hover:opacity-100! group-hover:opacity-40"
             href={item.url}
+            rel="noopener"
+            target="_blank"
           >
             <span className="w-22">{item.name}</span>
             <span className="flex-1 text-fg-3">{item.description}</span>
