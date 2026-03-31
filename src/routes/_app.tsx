942a08f234f17838dc14f52a9e95c2400cc1a48d
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2026-03-31 19:26:55 +0530

    chore: minor tweaks

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -12,7 +12,7 @@ function AppLayout() {
     <SidebarProvider>
       <div
         className={cn(
-          "px-4 pt-8 pb-floating-nav-inset sm:px-6 lg:pt-page-t lg:pb-8",
+          "px-4 pt-8 pb-floating-nav-inset sm:px-6 lg:pt-page-t lg:pb-page-t",
           "mx-auto max-w-[772px]",
           "lg:grid lg:max-w-7xl lg:grid-cols-[1fr_minmax(0,740px)_1fr] lg:gap-8"
         )}
