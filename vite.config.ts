1f0d7f3df70d12cdac1092c460ff2ed43c4fa0a6
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2026-04-18 15:38:58 +0530

    chore: use absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { config } from "@/lib/config";
+import { config } from "./src/lib/config"; // Absolute PATH on purpose
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
