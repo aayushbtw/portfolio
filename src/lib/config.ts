778e910b0b873e6319c47434690966b9547063c7
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2026-04-18 15:55:39 +0530

    chore(config): change env handling

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,7 +2,10 @@ export const config = {
   name: "Aayush Agarwal",
   description:
     "I build things for the web. Crafting interfaces that feel natural to use, with a focus on clarity.",
-  siteUrl: import.meta.env.DEV ? "http://localhost:3000" : "https://aayush.cv",
+  siteUrl:
+    process.env.NODE_ENV === "production"
+      ? "https://aayush.cv"
+      : "http://localhost:3000",
   socials: {
     github: "aayushbtw",
     twitter: "aayushbtw",
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { config } from "./src/lib/config"; // Absolute PATH on purpose
 
 export default defineConfig({
   resolve: { tsconfigPaths: true },
@@ -22,7 +21,10 @@ export default defineConfig({
       },
       sitemap: {
         enabled: true,
-        host: config.siteUrl,
+        host:
+          process.env.NODE_ENV === "production"
+            ? "https://aayush.cv"
+            : "http://localhost:3000",
       },
     }),
     viteReact(),
